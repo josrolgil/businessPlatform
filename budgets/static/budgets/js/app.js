6af0e87eb304d776a2707d7b123586a92a6f6a62
Pepe <josemaria.roldan@telcaria.com>
2018-12-16 11:04:42 +0100

    adding second address

--- budgets/static/budgets/js/app.js
+++ budgets/static/budgets/js/app.js
@@ -7,23 +7,31 @@ function myFunction() {
 }
 
 function addSecondDownloadAddress(addresses){
+    var obj = JSON.parse(addresses)
     var node = document.createElement('div');
+    var content = "";
     //node.innerHTML=
     //'<div class="form-group"><label class="col-sm-2 control-label">Select second download address</label><div class="dropdown col-sm-10"><div class="form-group"><select class="form-control" id="download2">{% for address in addresses %}<option>{{address.street}} {{address.city}}</option>{% endfor %}</select></div></div></div>'
-    node.innerHTML='<div class="form-group"><label class="col-sm-2 control-label">Select second download address</label><div class="dropdown col-sm-10"><div class="form-group"><select class="form-control" id="download2">'
+    content='<div class="form-group"><label class="col-sm-2 control-label">Select second download address</label><div class="dropdown col-sm-10"><div class="form-group"><select class="form-control" id="download2">'
+    alert(content)
+    for (j in obj){
+        content += "<option "
+        content += "value=\"" + obj[j].pk+"\">"
+        content += obj[j].fields.street + " " + obj[j].fields.city
+        content += "</option>"
+        alert(content)
+    }
+
 
 //    Object.keys(addresses).forEach(function(key){
-//        var node = document.createElement('option');
-//        alert(addresses['fields'])
-//        node.innerHTML+="<option>"
-//        node.innerHTML+=addresses[key]['street'] + " " + addresses[key]['city']
-//        node.innerHTML+="</option>"
+////        var node = document.createElement('option');
+//        alert(key)
+////        alert(addresses['fields'])
+////        node.innerHTML+="<option>"
+////        node.innerHTML+=addresses[key]['street'] + " " + addresses[key]['city']
+////        node.innerHTML+="</option>"
 //    })
-    alert(addresses)
-    json.forEach(function(obj['fields']){
-        alert(obj)
-
-    })
-    node.innerHTML+='</select></div></div></div>'
+    content += '</select></div></div></div>'
+    node.innerHTML = content
     document.getElementById("secondAddress").appendChild(node)
 }
